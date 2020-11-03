8aec130743f76a5c8b2ab3fefec83dd5c6fe32f7
ErikTrethewey-J <erikenobi@gmail.com>
2020-11-02 16:05:24 -0800

    Working on Project.

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,9 +37,10 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {userState} from 'react'; /* STEP 0 */
 
 export default function Spinner() {
+  const [spinnerOn, setSpinnerOn] = userState(true)
 /* STEP 1 */
 
   const toggleSpinner = () => {
